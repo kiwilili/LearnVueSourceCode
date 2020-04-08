2ade91ae944c5787ed0970eca9fc82747c7e34a2
kiwili <kiwilili@163.com>
2020-04-08 17:45:23 +0800

    Ad 高级异步组件

--- example/src/main.js
+++ example/src/main.js
@@ -28,11 +28,34 @@ Vue.config.productionTip = false
 //     resolve(res)
 //   })
 // })
+
 // promise
-Vue.component('HelloWorld',
-  // 该 'import' 函数返回一个 'Promise' 对象
-  () => import('./components/HelloWorld')
-)
+// Vue.component('HelloWorld',
+//   // 该 'import' 函数返回一个 'Promise' 对象
+//   () => import('./components/HelloWorld')
+// )
+
+// 高级异步组件
+const LodaingComp = {
+  template: '<div>loading</div>'
+}
+const ErrorComp = {
+  template: '<div>error</div>'
+}
+const AsyncComp = () => ({
+  // 需要加载的组件，总当是一个 Promise
+  component: import('./components/HelloWorld.vue'),
+  // 加载中应当渲染的组件
+  loading: LodaingComp,
+  // 出错时渲染的组件
+  error: ErrorComp,
+  // 渲染加载中组件前的等待时间。默认200ms
+  delay: 200,
+  // 最长等待时间，超出此时间则渲染错误组件，默认:Infinity
+  timeout: 1000
+})
+Vue.component('HelloWorld', AsyncComp)
+
 new Vue({
   el: '#app',
   render(h) {
